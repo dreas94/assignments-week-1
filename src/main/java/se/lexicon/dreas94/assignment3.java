85699bf8d6f8e98bd46ffa59903f8df97c380ba6
Andreas Eriksson <dreas94@gmail.com>
2022-03-21 11:28:35 +0100

    Finished Assignment 3 & Altered Assignment 2

--- src/main/java/se/lexicon/dreas94/assignment3.java
+++ src/main/java/se/lexicon/dreas94/assignment3.java
@@ -6,14 +6,29 @@ public class assignment3
     public static void main( String[] args )
     {
         Scanner sc= new Scanner(System.in);
-        System.out.print("Enter first digit: ");
-        double digit1 = sc.nextDouble();
-        System.out.print("Enter second digit: ");
-        double digit2 = sc.nextDouble();
+        double digit1 = 0;
+        double digit2 = 0;
 
-        System.out.print(digit1 + " + " + digit2 + " = " + (digit1+digit2) + "\n");
-        System.out.print(digit1 + " * " + digit2 + " = " + (digit1*digit2) + "\n");
-        System.out.print(digit1 + " / " + digit2 + " = " + (digit1/digit2) + "\n");
-        System.out.print(digit1 + " - " + digit2 + " = " + (digit1-digit2) + "\n");
+        boolean foundNumbers = false;
+
+        while(!foundNumbers)
+        {
+            System.out.print("Enter first digit: ");
+            if(sc.hasNextDouble())
+            {
+                digit1 = sc.nextDouble();
+                System.out.print("Enter second digit: ");
+                if(sc.hasNextDouble())
+                {
+                    digit2 = sc.nextDouble();
+                }
+                foundNumbers = true;
+            }
+        }
+
+        System.out.print(digit1 + " + " + digit2 + " = " + Math.round((digit1+digit2)*100)/100 + "\n" +
+                digit1 + " * " + digit2 + " = " + Math.round((digit1*digit2)*100)/100 + "\n" +
+                digit1 + " / " + digit2 + " = " + Math.round((digit1/digit2)*100)/100 + "\n" +
+                digit1 + " - " + digit2 + " = " + Math.round((digit1-digit2)*100)/100 + "\n");
     }
 }
