ace31dffc7bcba13994e42276beadf6e6dda5e7a
Andreas Eriksson <dreas94@gmail.com>
2022-03-21 10:32:42 +0100

    First Commit

---
+++ src/main/java/se/lexicon/dreas94/assignment1.java
@@ -0,0 +1,10 @@
+package se.lexicon.dreas94;
+
+public class assignment1
+{
+    public static void main( String[] args )
+    {
+        String name = "Andreas Eriksson";
+        System.out.println("Hello\n" + name);
+    }
+}
